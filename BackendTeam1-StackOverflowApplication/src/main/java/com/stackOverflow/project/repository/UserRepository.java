81682307b85dcebc27e2f16c5f432651189a66eb
Kiran Gowda <kinrgowda@gmail.com>
2022-02-15 16:57:25 +0530

    User_Table

--- BackendTeam1-StackOverflowApplication/src/main/java/com/stackOverflow/project/repository/UserRepository.java
+++ BackendTeam1-StackOverflowApplication/src/main/java/com/stackOverflow/project/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.stackOverflow.project.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.stackOverflow.project.model.User;
 
-public interface UserRepository extends JpaRepository<User, Long>{
 
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+		
 }
--- BackendTeam1-StackOverflowApplication/src/main/java/com/stackOverflow/project/service/UserService.java
+++ BackendTeam1-StackOverflowApplication/src/main/java/com/stackOverflow/project/service/UserService.java
@@ -1,5 +1,53 @@
 package com.stackOverflow.project.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.stackOverflow.project.model.User;
+import com.stackOverflow.project.repository.UserRepository;
+
+
+@Service
 public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	 public List<User> getuserList() {
+	        return userRepository.findAll();
+	    }
+
+	public User createUser(User user) {
+		return userRepository.save(user);
+	}
+	
+	public User getUserById(long id) {
+		return userRepository.findById(id).get();
+	}
+	
+	public String deleteStudentById(long id)
+	{
+		userRepository.deleteById(id);
+		return "user with ID: "+Long.toString(id)+" removed successfully";
+	}
+
+	public User updateUserDetails(User user, long id) {
+		User currentUser = userRepository.findById(id).orElse(null);
+		
+		currentUser.setUserName(user.getUserName());
+		currentUser.setLocation(user.getLocation());
+		currentUser.setEmail(user.getEmail());
+		currentUser.setPhone_Number(user.getPhone_Number());
+		currentUser.setAbout_Me(user.getAbout_Me());
+		currentUser.setUp_Votes(user.getUp_Votes());
+		currentUser.setDown_Votes(user.getDown_Votes());
+		currentUser.setRep_Score(user.getRep_Score());
+		currentUser.setLast_Acess_Date(user.getLast_Acess_Date());
+		
+		return userRepository.save(currentUser);
+	}
 
 }
